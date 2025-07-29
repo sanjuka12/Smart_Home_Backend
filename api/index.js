51f109cc8ccce34141ba4b3cc831ef4b45434c64
sanjuka12 <dishansanjuka3@gmail.com>
2025-07-29 11:27:23 +0530

    updated 3

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-const serverless = require('serverless-http');
-const app = require('../app'); // import the Express app
+// const serverless = require('serverless-http');
+// const app = require('../app'); // import the Express app
 
-module.exports = serverless(app); // export for Vercel
+// module.exports = serverless(app); // export for Vercel
+
+// // api/index.js
+
+// export default function handler(req, res) {
+//   res.status(200).json({ message: "Hello from API" });
+// }
+
+
+const serverless = require('serverless-http');
+const app = require('../app');
+
+module.exports = serverless(app);
