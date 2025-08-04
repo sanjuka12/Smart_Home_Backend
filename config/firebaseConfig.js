1f9c821352ea7a82f45bde2504ebff1f3d7408ef
sanjuka12 <dishansanjuka3@gmail.com>
2025-08-04 13:43:55 +0530

    Restored

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -20,27 +20,4 @@ const db = admin.firestore();
 
 module.exports = db;
 
-// const admin = require('firebase-admin');
 
-// // Safety check: make sure all required env variables are present
-// const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-// const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-// const projectId = process.env.FIREBASE_PROJECT_ID;
-
-// if (!privateKey || !clientEmail || !projectId) {
-//   console.error('❌ Missing Firebase environment variables');
-//   throw new Error('Missing Firebase config.');
-// }
-
-// // Initialize Firebase only once
-// if (!admin.apps.length) {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId,
-//       clientEmail,
-//       privateKey: privateKey.replace(/\\n/g, '\n'),
-//     }),
-//   });
-// }
-
-// module.exports = admin.firestore();
